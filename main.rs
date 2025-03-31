f178a25d78f4eca88932674defcf8dc1f5ec1881
Liamber1234 <liam.berguig@gmail.com>
2025-03-31 13:11:08 +0300

    hello - <Name> program

---
+++ main.rs
@@ -0,0 +1,8 @@
+use std::io;
+
+fn main() {
+    let mut name = String::new();
+    println!("Please enter your name:");
+    io::stdin().read_line(&mut name).expect("Failed to read line");
+    println!("Hello, {}!", name.trim());
+}
