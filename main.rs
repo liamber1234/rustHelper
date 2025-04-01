e6daab3e64326196f682eaf3dc84edb96f7ca58d
Liamber1234 <liam.berguig@gmail.com>
2025-04-01 10:06:43 +0300

    CR comments

--- main.rs
+++
@@ -1,8 +0,0 @@
-use std::io;
-
-fn main() {
-    let mut name = String::new();
-    println!("Please enter your name:");
-    io::stdin().read_line(&mut name).expect("Failed to read line");
-    println!("Hello, {}!", name.trim());
-}
