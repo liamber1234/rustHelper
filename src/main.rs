9bb0c92813ef80fcb49a1e823c5ad4e75a7c9c6e
Liamber1234 <liam.berguig@gmail.com>
2025-04-01 18:27:26 +0300

    Cr comments

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use std::process::exit;
 use std::result::Result;
+use std::cmp::Ordering;
 use std::io;
 use rand::Rng; // 0.8.5
 
-const MINIMUM_NUMBER: i32 = 1; // Minimum guess number
-const MAXIMUM_NUMBER: i32 = 101; // Maximum guess number
+/// Minimum guess number
+const MINIMUM_NUMBER: i32 = 1;
+
+/// Maximum guess number
+const MAXIMUM_NUMBER: i32 = 101;
 
 fn main() {
     let mut random_guess = String::new();    
@@ -15,33 +19,51 @@ fn main() {
         io::stdin().read_line(&mut random_guess).expect("failed");
         let input_number: i32 = random_guess.trim().parse().expect("Please enter a number");
         random_guess.clear();
-        check_range(input_number);
-        let result = check_guess(input_number, random);
-        if result.is_ok() {
-            println!("You guessed the number!");
-            break;
-        } else {
-            println!("{}", result.unwrap_err());
+        let mut result = check_range(input_number);
+        match result {
+            Ok(_) => {},
+            Err(ref e) => {
+                println!("{}", result.unwrap_err());
+                break;
+            }
+        }
+
+        result = check_guess_correct(input_number, random);
+        match result {
+            Ok(_) => {
+                println!("You guessed the number!");
+                break;
+            },
+            Err(ref e) => {
+                println!("{}", result.unwrap_err());
+            }
         }
         println!("Try again!");
     }   
 }
 
 /// Function to check if the guess is in the valid range
-fn check_range(guess : i32) {
+/// parameters:
+/// - guess: the number to check
+/// returns:
+/// - none
+fn check_range(guess : i32) -> Result<(), String> {
     if guess < MINIMUM_NUMBER || guess >= MAXIMUM_NUMBER {
-        println!("Number is not in the correct range - bye bye");
-        exit(0);
+        return Result::Err("Number is not in the correct range - bye bye".to_string());
     }
+    return Result::Ok(());
 }
 
 /// Function to check if the guess is correct
-fn check_guess(guess: i32, random: i32) -> Result<(), String> {
-    if guess == random {
-        return Result::Ok(());
-    } else if guess < random {
-        return Result::Err("Your guess is too low".to_string());
-    } else {
-        return Result::Err("Your guess is too high".to_string());
-    }
+/// parameters:
+/// - guess: the number to check
+/// - random: the number to check with
+/// returns:
+/// - Result: Ok if the guess is correct, Err if it is not
+fn check_guess_correct(guess: i32, random: i32) -> Result<(), String> {
+    match guess.cmp(&random) {
+        Ordering::Equal => return Result::Ok(()),
+        Ordering::Less => return Result::Err("Your guess is too low".to_string()),
+        Ordering::Greater => return Result::Err("Your guess is too high".to_string()),
+    }  
 }
