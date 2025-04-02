42291a561885ce70f38962dd38dc6cf73794211d
Liamber1234 <liam.berguig@gmail.com>
2025-04-02 13:31:22 +0300

    CR coments

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,9 @@ const MULTIPLY_COMMAND: &str = "*";
 const DIVIDE_COMMAND: &str = "/";
 
 fn main() {
-    let mut fisrt_input = String::new();
-    let mut second_input = String::new();
     let mut operation = String::new();
 
     println!("Rust Calculator");    
-
     // Get user input for the first number 
     println!("Enter first number: ");
     let first_number : i32 = input_number();
@@ -25,7 +22,7 @@ fn main() {
     println!("Choose operation: +, -, *, /:");
     io::stdin().read_line(&mut operation).expect("failed");
 
-    match operate(first_number, second_number, operation) {
+    match do_operate(first_number, second_number, operation) {
         Ok(result) => println!("The result is: {}", result),
         Err(_) => println!("Error: invalid input"),
     }
@@ -34,7 +31,7 @@ fn main() {
 
 
 /// This function takes inputs of user - (two numbers and an operation) and returns the result of the operation.
-fn operate(first_number: i32, second_number: i32, operation: String) -> Result<i32, bool> {
+fn do_operate(first_number: i32, second_number: i32, operation: String) -> Result<i32, ()> {
     //check the operaion type
     match operation.trim() {
         PLUS_COMMAND => return Result::Ok(first_number + second_number),
@@ -42,18 +39,21 @@ fn operate(first_number: i32, second_number: i32, operation: String) -> Result<i
         MULTIPLY_COMMAND => return Result::Ok(first_number * second_number),
         DIVIDE_COMMAND => {
             if second_number == 0 {
-                return Result::Err(false)
+                return Result::Err(());
             } else {
                 return Result::Ok(first_number / second_number)
             }
         },
-        _ => return Result::Err(false),
+        _ => return Result::Err(()),
     }
 }
 
+/// This function takes user input and returns the number as an i32.
+/// param input: the user input as a string
+/// return: the user input as an i32
 fn input_number() -> i32 {
     let mut input = String::new();
     io::stdin().read_line(&mut input).expect("failed");
     let number: i32 = input.trim().parse().expect("Please enter a number");
-    return number;
+    number
 }
