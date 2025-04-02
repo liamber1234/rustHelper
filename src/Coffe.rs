867b2fa1c4a42de34383affb727cf76502f1fe49
Liamber1234 <liam.berguig@gmail.com>
2025-04-02 17:02:48 +0300

    coffe order exercise

---
+++ src/Coffe.rs
@@ -0,0 +1,56 @@
+#[derive(Debug, Clone, Copy)]
+pub enum CoffeType {
+    Espresso,
+    Latte,
+    Cappuccino,
+    Americano,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum CoffeSize {
+    Small,
+    Medium,
+    Large,
+}
+
+pub struct CoffeOrder {
+    pub coffee_type: CoffeType,
+    pub coffee_size: CoffeSize,
+    pub has_sugar: bool,
+    pub sugar_amount: Option<u8>,
+}
+
+impl CoffeOrder {
+    /// Creates a new `CoffeOrder` instance.
+    /// parameters:
+    /// - coffe_type : the type of coffe
+    /// - coffe_size : the size of coffe
+    /// - has_sugar : if the coffe contains sugar
+    /// - sugar_amount : the amount of sugar in spoons, if any
+    /// returns: 
+    /// - a new CoffeOrder instance
+    pub fn new(coffee_type: CoffeType, coffee_size: CoffeSize, has_sugar: bool, sugar_amount: Option<u8>) -> Self {
+        CoffeOrder {
+            coffee_type,
+            coffee_size,
+            has_sugar,
+            sugar_amount,
+        }
+    }
+
+    /// Prints the details of the coffee order.
+    /// parameters:
+    /// - None
+    /// returns:
+    /// - None
+    pub fn print_order(&self) {
+        println!("Coffee Type: {:?}", self.coffee_type);
+        println!("Coffee Size: {:?}", self.coffee_size);
+        println!("Contains Sugar: {:?}", self.has_sugar);
+        if let Some(amount) = self.sugar_amount {
+            println!("Sugar Amount: {} spoons", amount);
+        } else {
+            println!("Sugar Amount: None");
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,141 @@
+use CoffeProj::Coffe::CoffeOrder;
+use CoffeProj::Coffe::CoffeSize;
+use CoffeProj::Coffe::CoffeType;
+
+fn main() {
+    let mut user_input = String::new();
+    let mut sugar_amount = 0;
+    let coffee_type = CoffeType::Espresso;
+    let mut coffee_size;
+    
+    loop {
+        print_type_menu();
+        user_input.clear();
+        if std::io::stdin().read_line(&mut user_input).is_err()
+        {
+            println!("Error reading input. Please try again.");
+            continue;
+        }
+        let coffee_type_input = user_input.trim();
+        
+        if convert_to_type(coffee_type_input).is_none() {
+            println!("Invalid coffee type. Please try again.");
+            continue;
+        }
+
+        print_size_menu();
+        user_input.clear();
+        if !std::io::stdin().read_line(&mut user_input).is_ok() {
+            println!("Error reading input. Please try again.");
+            continue;
+        }
+        let coffee_size_input = user_input.trim();
+
+        if let Some(size) = convert_to_size(coffee_size_input) {
+            coffee_size = size;  
+        } else {
+            println!("Invalid coffee size. Please try again.");
+            continue;
+        }
+
+        let sugar_result = handle_sugar_input();
+
+        let is_containg_sugar = sugar_result.is_some();
+        if is_containg_sugar {
+            match sugar_result {
+                Some(amount) => sugar_amount = amount,
+                None => println!("No sugar for you"),
+            };
+        }
+        
+        let order = CoffeOrder::new(coffee_type, coffee_size, is_containg_sugar, Some(sugar_amount));
+        order.print_order();
+    }
+}
+
+/// this function prints the coffee type menu
+/// parameters:
+/// - None
+/// returns:
+/// - None
+fn print_type_menu() {
+    println!("Select coffee type:");
+    println!("1. Espresso");
+    println!("2. Latte");
+    println!("3. Cappuccino");
+    println!("4. Americano");
+}
+
+/// this function gets string of number and converts it to CoffeType
+/// parameters:
+/// - coffee_type_input: the string input from the user
+/// returns:
+/// - Some(CoffeType) if the input is valid
+/// - None if the input is invalid
+fn convert_to_type(coffee_type_input: &str) -> Option<CoffeType> {
+    match coffee_type_input.trim() {
+        "1" => Some(CoffeType::Espresso),
+        "2" => Some(CoffeType::Latte),
+        "3" => Some(CoffeType::Cappuccino),
+        "4" => Some(CoffeType::Americano),
+        _ => None,
+    }
+}
+
+/// this function prints the coffee size menu
+/// parameters:
+/// - None
+/// returns:
+/// - None
+fn print_size_menu() {
+    println!("Select coffee size:");
+    println!("1. Small");
+    println!("2. Medium");
+    println!("3. Large");
+}
+
+/// this function gets string of number and converts it to CoffeSize
+/// parameters:
+/// - coffee_size_input: the string input from the user
+/// returns:
+/// - Some(CoffeSize) if the input is valid
+/// - None if the input is invalid
+fn convert_to_size(coffee_size_input: &str) -> Option<CoffeSize> {
+    match coffee_size_input.trim() {
+        "1" => Some(CoffeSize::Small),
+        "2" => Some(CoffeSize::Medium),
+        "3" => Some(CoffeSize::Large),
+        _ => None,
+    }
+}
+
+/// this function handles the sugar input from the user
+/// parameters:
+/// - None
+/// returns:
+/// - Some(sugar_amount) if the user wants sugar
+fn handle_sugar_input() -> Option<u8> {
+    let mut user_input = String::new();
+
+    println!("Do you want sugar? (y/n)");
+    if std::io::stdin().read_line(&mut user_input).is_err() {
+        println!("Error reading input. Please try again.");
+        return None;
+    }
+    let sugar_input = user_input.trim();
+    
+    match sugar_input.trim() {
+        "y" => {
+            println!("How many sugar cubes?");
+            let mut sugar_amount = String::new();
+            if std::io::stdin().read_line(&mut sugar_amount).is_err() {
+                println!("Error reading input. Please try again.");
+                return None;
+            }
+            let sugar_amount = sugar_amount.trim().parse::<u8>().ok();
+            sugar_amount
+        }
+        "n" => None,
+        _ => None,
+    }
+}
