114acef6ae5b56a40e25a3257105a1c79a146330
Liamber1234 <liam.berguig@gmail.com>
2025-04-07 12:19:43 +0300

    calculator after cr comments

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,10 @@ fn main() {
     println!("Choose operation: +, -, *, /:");
     io::stdin().read_line(&mut operation).expect("failed");
 
+    if check_validation(first_number, second_number, operation.clone()) == false {
+        println!("Error: invalid input");
+        return;
+    }
     match do_operate(first_number, second_number, operation) {
         Ok(result) => println!("The result is: {}", result),
         Err(_) => println!("Error: invalid input"),
@@ -33,19 +37,12 @@ fn main() {
 /// This function takes inputs of user - (two numbers and an operation) and returns the result of the operation.
 fn do_operate(first_number: i32, second_number: i32, operation: String) -> Result<i32, ()> {
     //check the operaion type
-    match operation.trim() {
-        PLUS_COMMAND => return Result::Ok(first_number + second_number),
-        MINUS_COMMAND => return Result::Ok(first_number - second_number),
-        MULTIPLY_COMMAND => return Result::Ok(first_number * second_number),
-        DIVIDE_COMMAND => {
-            if second_number == 0 {
-                return Result::Err(());
-            } else {
-                return Result::Ok(first_number / second_number)
-            }
-        },
-        _ => return Result::Err(()),
-    }
+    Result::Ok(match operation.trim() {
+        PLUS_COMMAND_var => first_number + second_number,
+        MINUS_COMMAND_var => first_number - second_number,
+        MULTIPLY_COMMAND_var => first_number * second_number,
+        DIVIDE_COMMAND_var => first_number / second_number,
+    })
 }
 
 /// This function takes user input and returns the number as an i32.
@@ -56,4 +53,16 @@ fn input_number() -> i32 {
     io::stdin().read_line(&mut input).expect("failed");
     let number: i32 = input.trim().parse().expect("Please enter a number");
     number
+}
+
+fn check_validation(first_number: i32, second_number: i32, operation: String) -> bool {
+    if operation.trim() == DIVIDE_COMMAND && second_number == 0 {
+        return false; // Division by zero is not allowed
+    }
+    
+    if operation.trim() != PLUS_COMMAND && operation.trim() != MINUS_COMMAND && operation.trim() != MULTIPLY_COMMAND && operation.trim() != DIVIDE_COMMAND {
+        return false; // Invalid operation
+    }
+
+    return true;
 }
